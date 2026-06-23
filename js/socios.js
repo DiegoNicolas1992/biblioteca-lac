3b39f31431708aa9f50efd60be36469b0ee60dd6
DiegoNicolas1992 <cuellodiego333@gmail.com>
2026-06-23 09:37:31 -0300

    Update socios.js

--- js/socios.js
+++ js/socios.js
@@ -1,41 +1,41 @@
-function guardarSocio(){
+alert("socios.js cargado correctamente");
 
-let dni = document.getElementById("dni").value;
-let nombre = document.getElementById("nombre").value;
-let apellido = document.getElementById("apellido").value;
-let telefono = document.getElementById("telefono").value;
+function guardarSocio() {
 
-if(
-dni === "" ||
-nombre === "" ||
-apellido === "" ||
-telefono === ""
-){
-alert("Complete todos los campos");
-return;
-}
+    alert("Entró a la función");
 
-let socios =
-JSON.parse(localStorage.getItem("socios")) || [];
+    let dni = document.getElementById("dni").value;
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
 
-socios.push({
-dni,
-nombre,
-apellido,
-telefono
-});
+    if (
+        dni === "" ||
+        nombre === "" ||
+        apellido === "" ||
+        telefono === ""
+    ) {
+        alert("Complete todos los campos");
+        return;
+    }
 
-localStorage.setItem(
-"socios",
-JSON.stringify(socios)
-);
+    let socios =
+        JSON.parse(localStorage.getItem("socios")) || [];
 
-document.getElementById("mensaje").innerText =
-"Socio registrado correctamente";
+    socios.push({
+        dni,
+        nombre,
+        apellido,
+        telefono
+    });
 
-document.getElementById("dni").value="";
-document.getElementById("nombre").value="";
-document.getElementById("apellido").value="";
-document.getElementById("telefono").value="";
+    localStorage.setItem(
+        "socios",
+        JSON.stringify(socios)
+    );
 
+    document.getElementById("mensaje").innerText =
+        "Socio registrado correctamente";
+
+    alert("Guardado correctamente");
 }
