94a3433d263732412eedbae5d9daf5c1ca404b79
DiegoNicolas1992 <cuellodiego333@gmail.com>
2026-06-23 09:30:04 -0300

    Update socios.js

--- js/socios.js
+++ js/socios.js
@@ -1,1 +1,41 @@
+function guardarSocio(){
 
+let dni = document.getElementById("dni").value;
+let nombre = document.getElementById("nombre").value;
+let apellido = document.getElementById("apellido").value;
+let telefono = document.getElementById("telefono").value;
+
+if(
+dni === "" ||
+nombre === "" ||
+apellido === "" ||
+telefono === ""
+){
+alert("Complete todos los campos");
+return;
+}
+
+let socios =
+JSON.parse(localStorage.getItem("socios")) || [];
+
+socios.push({
+dni,
+nombre,
+apellido,
+telefono
+});
+
+localStorage.setItem(
+"socios",
+JSON.stringify(socios)
+);
+
+document.getElementById("mensaje").innerText =
+"Socio registrado correctamente";
+
+document.getElementById("dni").value="";
+document.getElementById("nombre").value="";
+document.getElementById("apellido").value="";
+document.getElementById("telefono").value="";
+
+}
